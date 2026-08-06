d753d8f19e9a6026e4a39e080502758442eb252e
Shihab Mahamud <shihab4t@gmail.com>
2026-08-06 12:25:06 +0600

    wip

--- resources/js/components/layouts/app-layout/quick-action.tsx
+++ resources/js/components/layouts/app-layout/quick-action.tsx
@@ -1,18 +1,26 @@
-import { Button } from '@/components/ui/button';
 import {
     Tooltip,
     TooltipContent,
     TooltipTrigger,
 } from '@/components/ui/tooltip';
+
+import { Button } from '@/components/ui/button';
 import { PlusIcon } from 'lucide-react';
+import { Link } from '@inertiajs/react';
 
 export const QuickAction = () => {
     return (
         <Tooltip>
-            <TooltipTrigger>
-                <Button className="absolute right-10 bottom-10" size="icon-lg">
-                    <PlusIcon className="size-8" />
-                </Button>
+            <TooltipTrigger
+                className="absolute right-10 bottom-10"
+                render={
+                    <Button
+                        size="icon-lg"
+                        render={<Link href="/transactions/create" />}
+                    />
+                }
+            >
+                <PlusIcon className="size-8" />
             </TooltipTrigger>
             <TooltipContent>Add new Transaction</TooltipContent>
         </Tooltip>
