0aeb3afd6e8c4d58b892a9c11ea97fd64bc131b6
Shihab Mahamud <shihab4t@gmail.com>
2026-08-06 10:58:50 +0600

    wip

--- resources/js/components/layouts/app-layout/user-menu-content.tsx
+++ resources/js/components/layouts/app-layout/user-menu-content.tsx
@@ -34,11 +34,13 @@ export const UserMenuContent = ({ user }: { user: TUser }) => {
 
     return (
         <>
-            <DropdownMenuLabel className="p-0 font-normal">
-                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                    <UserInfo user={user} showEmail={true} />
-                </div>
-            </DropdownMenuLabel>
+            <DropdownMenuGroup>
+                <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                        <UserInfo user={user} showEmail={true} />
+                    </div>
+                </DropdownMenuLabel>
+            </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
                 <DropdownMenuItem
