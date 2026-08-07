17d0c92d8d5ccd042cb823a3c79adf856cdf504e
Shihab Mahamud <shihab4t@gmail.com>
2026-08-07 18:07:38 +0600

    feat: add dark/light logo variants with black and white wordmark text

--- resources/js/components/layouts/app-layout/app-logo.tsx
+++ resources/js/components/layouts/app-layout/app-logo.tsx
@@ -1,5 +1,11 @@
 import logoFull from '@/assets/logo-full.svg';
+import logoFullDark from '@/assets/logo-full-dark.svg';
 
 export const AppLogo = () => {
-    return <img src={logoFull} alt="Spendr" className="h-8 w-auto" />;
+    return (
+        <>
+            <img src={logoFull} alt="Spendr" className="h-8 w-auto dark:hidden" />
+            <img src={logoFullDark} alt="Spendr" className="h-8 w-auto hidden dark:block" />
+        </>
+    );
 };
