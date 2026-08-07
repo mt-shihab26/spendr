6c55134a33710f54a3189d6b90b5740ad8129cdb
Shihab Mahamud <shihab4t@gmail.com>
2026-08-07 18:33:16 +0600

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,9 +22,10 @@
                 background-color: oklch(0.145 0 0);
             }
         </style>
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
         @routes
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
