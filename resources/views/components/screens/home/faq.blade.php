b76fa575fd181b2ab4575020e6e22dca49988608
Shihab Mahamud <shihab4t@gmail.com>
2026-08-11 02:50:53 +0600

    extract inline SVGs into icon components
    Add volume-x icon component and replace inline SVGs in faq and video-showcase with x-icons.plus and x-icons.volume-x.

--- resources/views/components/screens/home/faq.blade.php
+++ resources/views/components/screens/home/faq.blade.php
@@ -56,9 +56,7 @@ $faqs = [
                         <span
                             class="shrink-0 text-muted-foreground transition-transform duration-200 group-open:rotate-45"
                         >
-                            <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                            </svg>
+                            <x-icons.plus class="size-4" />
                         </span>
                     </summary>
                     <p class="mt-4 text-sm leading-relaxed text-muted-foreground">{{ $faq['answer'] }}</p>
