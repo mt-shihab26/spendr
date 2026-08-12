f025240b690037a4ed0c60d4c0c4c34e02b3a300
Shihab Mahamud <shihab4t@gmail.com>
2026-08-12 14:03:28 +0600

    fix ExampleTest by faking ipify.org HTTP call to avoid network timeout

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
+
 test('returns a successful response', function () {
+    Http::fake(['https://api.ipify.org' => Http::response('1.2.3.4')]);
+
     $response = $this->get(route('home'));
 
     $response->assertOk();
